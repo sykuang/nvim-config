97fc32141dc9619aa82fc97c3884c267475fbaf9
sykuang <sykuang.tw@gmail.com>
2021-12-29 00:42:08 +0800

    Use custom config for telescope

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -33,7 +33,9 @@ M.plugins = {
    -- this string will be called in a `require`
    --              use "(custom.configs).my_func()" to call a function
    --              use "custom.blankline" to call a file
-   default_plugin_config_replace = {},
+   default_plugin_config_replace = {
+      telescope = "custom.plugins.telescope"
+   },
 }
 
 return M
