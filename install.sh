68b6aa20082e6fbc31f5f34c379a3cd06a71c4f1
sykuang <sykuang.tw@gmail.com>
2022-11-22 11:53:03 +0800

    Refine install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,9 @@ set -o errexit    # exit when command fails
 
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-if command -v nvim &> /dev/null
-then
-  if [[ ! -d  ~/.config/nvim ]];then
-    git clone https://github.com/NvChad/NvChad ~/.config/nvim
-  fi
-  rm -rf ~/.config/nvim/lua/custom 
-  ln -s "$SCRIPT_PATH/custom" ~/.config/nvim/lua/custom
-  nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
+if [[ ! -d  ~/.config/nvim ]];then
+  git clone https://github.com/NvChad/NvChad ~/.config/nvim
 fi
+rm -rf ~/.config/nvim/lua/custom
+ln -s "$SCRIPT_PATH/custom" ~/.config/nvim/lua/custom
+nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
