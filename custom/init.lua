188bbdd5daa72e293e79179954d834643625f9de
sykuang <sykuang.tw@gmail.com>
2023-02-20 16:12:10 +0800

    update to nvchad breaking change for v2.0

--- custom/init.lua
+++ custom/init.lua
@@ -1,8 +1,7 @@
-require "custom.mappings"
 local opt = vim.opt
 opt.mouse = "v"
-vim.cmd [[
-set spelllang=en,cjk
-set spellsuggest=best,9
-]]
+-- vim.cmd [[
+-- set spelllang=en,cjk
+-- set spellsuggest=best,9
+-- ]]
 vim.opt.clipboard = ""
