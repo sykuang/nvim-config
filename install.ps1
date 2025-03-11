d2f14ac040ebb47b5397336da27ed150d3d07f16
SYKuang <sykuang.tw@gmail.com>
2025-03-11 16:31:15 +0800

    feat(install): Add PowerShell Editor Services download and setup feat(lspconfig): Enhance LSP setup for PowerShell with custom command

--- install.ps1
+++ install.ps1
@@ -1,11 +1,21 @@
 $SCRIPT_PATH = $MyInvocation.MyCommand.Path
 $SCRIPT_FOLDER = Split-Path $SCRIPT_PATH -Parent
 $NVCHAD_PATH = "$HOME\\AppData\\Local\\nvim"
+$PSES_URL = "https://github.com/PowerShell/PowerShellEditorServices/releases/download/v4.2.0/PowerShellEditorServices.zip"
+$PSES_ZIP = Join-Path $HOME "Downloads\PowerShellEditorServices.zip"
+$PSES_FOLDER = Join-Path $HOME ".PSES"
 
 # Check if it's running with Administrator privileges, if it's not, re-run the script with elevated privileges
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)) {
     Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$SCRIPT_PATH`"" -Verb RunAs
     exit
+}
+
+# Download and extract PowerShellEditorServices
+if (-not (Test-Path $PSES_FOLDER)) {
+    Invoke-WebRequest -Uri $PSES_URL -OutFile $PSES_ZIP
+    Expand-Archive -Path $PSES_ZIP -DestinationPath  $PSES_FOLDER 
+    Remove-Item $PSES_ZIP
 }
 
 New-Item -ItemType SymbolicLink -Path $NVCHAD_PATH -Target $SCRIPT_FOLDER\
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,29 +1,34 @@
 -- load defaults i.e lua_lsp
-require("nvchad.configs.lspconfig").defaults()
+local nvlsp = require "nvchad.configs.lspconfig"
+local lspconfig = require "lspconfig"
 
-local lspconfig = require("lspconfig")
+nvlsp.defaults() -- loads nvchad's defaults
 
 local function is_windows()
-	return vim.loop.os_uname().version:match("Windows")
+    return vim.loop.os_uname().version:match("Windows")
 end
 -- lspservers with default config
-local servers = {
-	"clangd",
-	"yamlls",
-	"pylsp",
-	"bashls",
-	"jsonls",
-	"dockerls",
-	"ts_ls",
-}
+local servers = {"clangd", "yamlls", "pylsp", "jsonls", "dockerls", "ts_ls"}
 if is_windows() then
-  servers.insert("powershell_es")
+    table.insert(servers, "powershell_es")
+    table.insert(servers, "csharp_ls")
+else
+    table.insert(servers, "bashls")
 end
-local nvlsp = require "nvchad.configs.lspconfig"
 for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup({
-		on_attach = nvlsp.on_attach,
-		on_init = nvlsp.on_init,
-		capabilities = nvlsp.capabilities,
-	})
+    if lsp == "powershell_es" then
+        lspconfig[lsp].setup({
+            on_attach = nvlsp.on_attach,
+            on_init = nvlsp.on_init,
+            capabilities = nvlsp.capabilities,
+            cmd = {'pwsh', '-NoLogo', '-NoProfile', '-Command',
+                   [[& "$HOME\.PSES\PowerShellEditorServices\Start-EditorServices.ps1"]]}
+        })
+    else
+        lspconfig[lsp].setup({
+            on_attach = nvlsp.on_attach,
+            on_init = nvlsp.on_init,
+            capabilities = nvlsp.capabilities
+        })
+    end
 end
